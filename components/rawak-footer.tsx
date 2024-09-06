90947263666273919ce109ecd884bb2637240ddb
alserembani94 <alserembani94@gmail.com>
2024-09-06 11:33:27 +0800

    feat: add get user location (of course with permission

--- components/rawak-footer.tsx
+++ components/rawak-footer.tsx
@@ -3,8 +3,28 @@ import React from "react";
 import { Button } from "./ui/button";
 
 const RawakFooter = () => {
+	const handleGetLocation = () => {
+		navigator.geolocation.getCurrentPosition(
+			(position) => {
+				console.log("position", position);
+				const { latitude, longitude } = position.coords;
+				console.log([latitude, longitude]);
+			},
+			(error) => {
+				console.error("Error getting location:", error);
+			},
+		);
+	};
+
 	return (
-		<footer className="h-8 w-full p-8 flex items-center justify-center fixed bottom-0 left-0 right-0 bg-background border-t-2 border-border shadow-sm">
+		<footer className="h-8 w-full p-8 flex items-center justify-center fixed bottom-0 left-0 right-0 bg-background border-t-2 border-border shadow-sm gap-4">
+			<Button
+				variant="outline"
+				className="bg-gradient-to-br from-orange-500 to-orange-300 border border-orange-400 rounded-full text-background font-medium"
+				onClick={handleGetLocation}
+			>
+				<p>Dapatkan masjid terdekat</p>
+			</Button>
 			<Link href="/rawak">
 				<Button
 					variant="outline"
